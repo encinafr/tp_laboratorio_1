b0977227acd18bc720d1eb9fa1d607d0e6f19d4a
encinafr <36193725+encinafr@users.noreply.github.com>
2018-04-09 21:44:43 -0300

    tp_laboratorio_1

--- TP_1_Cascara/funciones.c
+++ TP_1_Cascara/funciones.c
@@ -1,34 +1,60 @@
-//esta funcion suma dos numeros
+ /** \brief esta funcion realiza la suma de dos numeros
+*
+* \param n1 es un numero
+* \param n2 es un numero
+* \return devuelve la suma de los valores de n1 y n2
+*/
 int suma(int n1, int n2){
     int resultado;
 
     resultado= n1 + n2;
   return resultado;
 }
-//esta funcion realiza la multiplicacion de dos numeros
+ /** \brief esta funcion realiza la multiplicacion de dos numeros
+*
+* \param n1 es un numero
+* \param n2 es un numero
+* \return devuelve la multiplicacion de los valores de n1 y n2
+*/
 int multiplicacion(int n1,int n2){
 int resultado;
 resultado=n1*n2;
 return resultado;
 }
-//esta funcion realiza la resta de dos numeros
+ /** \brief esta funcion realiza la resta de dos numeros
+*
+* \param n1 es un numero
+* \param n2 es un numero
+* \return devuelve la resta de los valores de n1 y n2
+*/
+
 int resta(int n1, int n2){
    int resultado;
    resultado=n1-n2;
    return resultado;
 }
-//esta funcion realizala division de dos numeros
+ /** \brief esta funcion realiza la division de dos numeros
+*
+* \param n1 es un numero
+* \param n2 es un numero
+* \return devuelve el cociente de los valores de n1 y n2
+*/
 float division(float n1, float n2){
     float resultado;
      while(n2==0){
                     printf("ERROR, no es posible la division por cero \n");
                     printf("Ingrese el segundo operando\n ");
+
                     scanf("%f",&n2);
                 }
     resultado=n1/n2;
     return resultado;
 }
-//esta funcion calcula el factorial de un numero
+ /** \brief esta funcion realiza el factorial de un numero entero
+*
+* \param n1 es un numero entero
+* \return devuelve el factorial del valor numerico de n1
+*/
 int factorial(int valor){
 int resultado=1 ,i;
      if(valor<0){
--- TP_1_Cascara/main.c
+++ TP_1_Cascara/main.c
@@ -6,10 +6,9 @@ int main()
 {
     char seguir='s';
     int opcion=0;
-    float numero1;
-    float numero2;
+    float n1;
+    float n2;
     float resultado;
-
 
 
     while(seguir=='s')
@@ -17,8 +16,8 @@ int main()
     {
         system("cls");
 
-        printf("1- Ingresar 1er numero (A=x)\n");
-        printf("2- Ingresar 2do numero (B=x)\n");
+        printf("1- Ingresar 1er numero (A=%.f)\n",n1);
+        printf("2- Ingresar 2do numero (B=%.f)\n",n2);
         printf("3- Calcular la suma (A+B)\n");
         printf("4- Calcular la resta (A-B)\n");
         printf("5- Calcular la division (A/B)\n");
@@ -34,68 +33,68 @@ int main()
             case 1:
 
                 printf("Ingrese el primer operando \n");
-                scanf("%f",&numero1);
+                scanf("%f",&n1);
 
                 break;
 
             case 2:
                 printf("Ingrese el segundo operando\n ");
-                scanf("%f",&numero2);
+                scanf("%f",&n2);
                 break;
             case 3:
-                resultado=suma(numero1,numero2);
+                resultado=suma(n1,n2);
                 printf("La suma es: %.0f \n",resultado);
                 system("pause");
 
                 break;
             case 4:
-                resultado=resta(numero1,numero2);
+                resultado=resta(n1,n2);
                 printf("La resta es: %.0f \n", resultado);
                 system("pause");
                 break;
             case 5:
-                while(numero2==0){
+                while(n2==0){
                     printf("ERROR, no es posible la division por cero \n");
                     printf(" Ingrese el segundo operando\n ");
-                    scanf("%f",&numero2);
+                    scanf("%f",&n2);
                 }
-                resultado= division(numero1,numero2);
+                resultado= division(n1,n2);
                 printf("El resultado de la division es %.2f \n ",resultado);
                 system("pause");
                 break;
             case 6:
-                resultado=multiplicacion(numero1,numero2);
+                resultado=multiplicacion(n1,n2);
                 printf("El resultado de la multiplicacion es: %.0f \n " , resultado);
                 system("pause");
                 break;
             case 7:
-                if(numero1>=0){
-                resultado= factorial(numero1);
+                if(n1>=0){
+                resultado= factorial(n1);
                 printf("El factorial del numero ingresado es: %.0f \n",resultado);
                 system("pause");
                  }else{
-                     resultado= factorial(numero1);
+                     resultado= factorial(n1);
                  }
                 break;
             case 8:
-                resultado=suma(numero1,numero2);
+                resultado=suma(n1,n2);
                 printf("La suma es: %.0f \n",resultado);
 
-                resultado=resta(numero1,numero2);
+                resultado=resta(n1,n2);
                 printf("La resta es: %.0f \n", resultado);
 
-                resultado= division(numero1,numero2);
+                resultado= division(n1,n2);
                 printf("El resultado de la division es: %.2f\n ",resultado);
 
-                resultado=multiplicacion(numero1,numero2);
+                resultado=multiplicacion(n1,n2);
                 printf("El resultado de la multiplicacion es: %.0f\n " , resultado);
 
-                if(numero1>=0){
-                resultado= factorial(numero1);
+                if(n1>=0){
+                resultado= factorial(n1);
                 printf("El factorial del numero ingresado es: %.0f\n",resultado);
                 system("pause");
                  }else{
-                     resultado= factorial(numero1);
+                     resultado= factorial(n1);
                  }
 
                 break;
